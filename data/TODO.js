8175b47be22ae67c4ec0817921d9c5c2c862fd95
Triana Cerda <trianaRcerda@gmail.com>
2021-08-23 14:12:00 -0700

    added todo.js + kept original animal.js for test purposes + todo has 3 tasks for user_id:1

--- data/TODO.js
+++ data/TODO.js
@@ -4,5 +4,18 @@ module.exports = [
     todo: 'feed the animals',
     completed: false,
     user_id: 1
+  },
+  {
+    id: 2,
+    todo: 'take out the trash',
+    completed: false,
+    user_id: 1
+  },
+  {
+    id: 3,
+    todo: 'clean the litter box',
+    completed: false,
+    user_id: 1
   }
 ];
+  
